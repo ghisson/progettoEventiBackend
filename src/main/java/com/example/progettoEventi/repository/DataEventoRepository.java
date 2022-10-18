ab6e3ee820a311315fcb175e2e1cafa43a4e9e61
Marcello <marcello.dechirico1@gmail.com>
2022-10-18 18:28:58 +0200

    mi ha dato fastidio

--- src/main/java/com/example/progettoEventi/repository/DataEventoRepository.java
+++ src/main/java/com/example/progettoEventi/repository/DataEventoRepository.java
@@ -1,10 +1,13 @@
 package com.example.progettoEventi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.progettoEventi.model.DataEvento;
+import com.example.progettoEventi.model.Evento;
 
 
 public interface DataEventoRepository  extends JpaRepository<DataEvento, Long> {
-
+	List<DataEvento> findByEvento(Evento evento);
 }
--- src/main/java/com/example/progettoEventi/repository/SettoreDataEventoRepository.java
+++ src/main/java/com/example/progettoEventi/repository/SettoreDataEventoRepository.java
@@ -1,11 +1,14 @@
 package com.example.progettoEventi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.progettoEventi.model.DataEvento;
 import com.example.progettoEventi.model.SettoreDataEvento;
 
 public interface SettoreDataEventoRepository extends JpaRepository<SettoreDataEvento, Long> {
 	
-
+	List<SettoreDataEvento> findByDataEvento(DataEvento dataEvento);
 
 }
